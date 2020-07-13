00f2e3f2a673e80b2830153764bffcf62ba67f9d
pagos21 <paga@localhost.yeah>
2020-07-13 16:15:04 +0200

    v 0.6(wip + js data)

--- JS/script.js
+++ JS/script.js
@@ -5,10 +5,14 @@ $(document).ready(function() {
 
   $("#inp").keyup(function(event){
     var input = $("#inp").val();
+    var date = new Date();
+    var dateHM = date.toLocaleTimeString();
     console.log(input);
-    if (event.keyCode == 13) {
-      $(".main").append("<div class='u_sender'> <p></p> </div>");
-      $(".u_sender p").last().text(input);
+    if (event.keyCode == 13 && input != "") {
+      $(".main").append("<div class='u_sender'> <p class = 'txt'></p> <p class = 'sendDate'></p>  </div>");
+      $(".u_sender p.txt").last().text(input);
+      $(".u_sender p.sendDate").last().text(dateHM);
+
       $("#inp").val("");
     }
   })
