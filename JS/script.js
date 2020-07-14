dc156b19380a756e828e652d4e1d5f424c748d7b
pagos21 <paga@localhost.yeah>
2020-07-14 14:58:21 +0200

    v 1.0

--- JS/script.js
+++ JS/script.js
@@ -1,20 +1,41 @@
-
-
-
 $(document).ready(function() {
+
+
+
+  var date = new Date();
+  var dateHM = date.toLocaleTimeString();
 
   $("#inp").keyup(function(event){
     var input = $("#inp").val();
-    var date = new Date();
-    var dateHM = date.toLocaleTimeString();
     console.log(input);
     if (event.keyCode == 13 && input != "") {
       $(".main").append("<div class='u_sender'> <p class = 'txt'></p> <p class = 'sendDate'></p>  </div>");
       $(".u_sender p.txt").last().text(input);
       $(".u_sender p.sendDate").last().text(dateHM);
-
       $("#inp").val("");
+      setTimeout(function(){
+        reply();
+      }, 1000);
     }
   })
 
+  search();
+
+
+
+
+
+  function reply(){
+    $(".main").append("<div class='receive'> <p class = 'txt'></p> <p class = 'sendDate'></p>  </div>");
+    $(".receive p.txt").last().text("Follow the white rabbit!");
+    $(".receive p.sendDate").last().text(dateHM);
+  }
+  function search(){
+    $("#searchContacts").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $(".list .contacts_list").filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  }
 })
