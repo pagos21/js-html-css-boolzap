d585b2af7054b9d16248eaa2f224addc75b09909
pagos21 <paga@localhost.yeah>
2020-07-14 15:45:25 +0200

    v 1.0.3 + comments

--- JS/script.js
+++ JS/script.js
@@ -10,9 +10,11 @@ $(document).ready(function() {
     console.log(input);
     if (event.keyCode == 13 && input != "") {
       $(".main").append("<div class='u_sender'> <p class = 'txt'></p> <p class = 'sendDate'></p>  </div>");
+      // Inserisco il testo solo all'ultimo div creato lasciando stare quelli precedenti
       $(".u_sender p.txt").last().text(input);
       $(".u_sender p.sendDate").last().text(dateHM);
       $("#inp").val("");
+      // Dopo 1 sec eseguo questa funzione
       setTimeout(function(){
         reply();
       }, 1000);
@@ -34,6 +36,8 @@ $(document).ready(function() {
     $("#searchContacts").on("keyup", function() {
       var value = $(this).val().toLowerCase();
       $(".list .contacts_list").filter(function() {
+        // Restituisce -\ (false) se la il valore non è presente altrimenti stampa la posizione
+        // Con toggle vengono nascosti solo gli elementi -1 (che non soddisfano il criterio)
         $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
         console.log($(this).text().toLowerCase().indexOf(value));
         console.log($(this).text().toLowerCase().indexOf(value) > -1);
